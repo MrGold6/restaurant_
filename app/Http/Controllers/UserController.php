ba892fa58e8c749773c42a3fb5a7172142f8b355
Evgengit <ev.mazur27@gmail.com>
2021-10-12 18:56:57 +0300

    DishServiceImp::getAll already return associative array where key is a dish group

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use App\Service\TableServiceImp;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
 class UserController extends Controller
