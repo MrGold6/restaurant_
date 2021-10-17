42739a43d67944a7cdecb581bc885ed43f134550
tymosh4uk <nazariytymoschuk@gmail.com>
2021-10-17 22:24:27 +0300

    make footer

--- resources/views/user/includes/footer.blade.php
+++ resources/views/user/includes/footer.blade.php
@@ -1,5 +1,92 @@
 @section('footer')
-<section id="footer"></section>
+<section id="footer">
 <footer style="margin-top: 100px">
-    <h2 style="text-align: center; margin-top:20px; margin-bottom: 20px;">Here is footer</h2>
+    <div class="footer">
+        <div class="footer-container">
+            <div class="footer__logo__container">
+                <a class="footer__link">
+                    <img src="{{ URL::to('/photos/logo.png') }}" alt="logo">
+                </a>
+            </div>
+            <div class="aboutFooter-links">
+                <h3 class="footer-title">Navigate</h3>
+                <ul class="footer-info-list">
+                    <li>
+                        <a href="#">Головна</a>
+                    </li>
+                    <li>
+                        <a href="#white-section1">Про нас</a>
+                    </li>
+                    <li>
+                        <a href="#menu">Меню</a>
+                    </li>
+                    <li>
+                        <a href="#booking">Бронь столика</a>
+                    </li>
+                    <li>
+                        <a href="#add_info_wr">Заходи</a>
+                    </li>
+                    <li>
+                        <a href="#footer">Контакти</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="footerSectionOne ">
+                <h3 class="footer-title">INFO</h3>
+                <div class="footerStore-description">
+                About restaurant goes here
+                <div data-content-region="home_footer_company_info"></div>
+                </div><!--.footerStore-description ends-->
+                <div class="footerStore-details">
+                <ul>
+
+                    <li>
+                        <div class="storeDetail-icon">
+                        <svg height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 274.64 384.38">
+                            <path d="M154.59,5.54c7,1.19,14.1,2,21,3.64C218,19.52,248.64,44.83,267.74,83.67c19.89,40.44,18.51,81.58,1.31,122.65-9.33,22.29-22.32,42.45-36.56,61.82-17.7,24.08-36,47.71-53.26,72.12-10,14.15-18.44,29.41-27.45,44.25-1.7,2.79-3.21,5.43-6.93,5.41-3.54,0-5.2-2.43-6.65-5.22-16.16-31.08-36.83-59.18-57.6-87.22-16.52-22.31-33.31-44.43-47.14-68.59-11.37-19.86-20.68-40.6-24-63.42C2.14,115.41,17.29,73.07,54.94,39.3c22-19.7,48.16-30.46,77.6-33.14a15.06,15.06,0,0,0,2.53-.62ZM216.9,142.88A72,72,0,1,0,145.19,215,72.09,72.09,0,0,0,216.9,142.88Z" transform="translate(-7.67 -5.54)"></path>
+                        </svg>
+                        </div><!--.storeDetail-icon end-->
+
+                        <div class="storeDetail">
+                        <a href="http://maps.google.com/maps?q=123 Widget Street Acmeville, AC 12345" aria-label="Store address: 123 Widget Street Acmeville, AC 12345">123 Widget Street Acmeville, AC 12345</a>
+                        </div><!--.storeDetail ends-->
+                    </li>
+
+                    <li>
+                        <div class="storeDetail-icon">
+                        <svg height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 306.59 306.55">
+                            <path d="M85.24,6c4.6,1.57,9.14,3.49,11.33,8.11a24.56,24.56,0,0,1,2.59,9.64A209.15,209.15,0,0,0,109,84c2.19,6.79.17,12.76-4.84,17.77-11.63,11.66-23.22,23.36-34.89,35-1.42,1.4-1.78,2.39-.77,4.29q38,71.46,109.61,109.09c.44.23.9.43,2.06,1a17.12,17.12,0,0,1,1.77-2.51c11.33-11.4,22.73-22.72,34-34.16,5.62-5.69,12-7.34,19.65-4.88a195.67,195.67,0,0,0,56.15,9.32,33.49,33.49,0,0,1,10.77,1.87c5.46,2,8,6.91,9.68,12.19v65.87a7.1,7.1,0,0,0-.58,1c-3,8.71-8.79,13-18,12.72s-18.16-.53-27.19-1.4A282.09,282.09,0,0,1,197,295.47,288,288,0,0,1,89.3,226.59a287.09,287.09,0,0,1-61.94-93.36A289.26,289.26,0,0,1,5.61,24.57c-.07-7.7,3-13.49,10-17C16.81,7,18.11,6.53,19.37,6Z" transform="translate(-5.61 -6)"></path>
+                        </svg>
+                        </div>
+
+                        <div class="storeDetail">
+                            <a href="#">
+                            +38 068 096 49 49
+                            </a>
+                        </div>
+                    </li>
+
+                    <li>
+                    <div class="storeDetail-icon">
+                        <svg height="10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 297 157.14">
+                        <path d="M83.56,128.45,4.47,88.87C2,87.63,0,88.88,0,91.63v122c0,2.75,1.59,3.4,3.54,1.46l80.95-80.9A3.39,3.39,0,0,0,83.56,128.45Z" transform="translate(0 -69.93)"></path>
+                        <path d="M196.39,142.29a7.58,7.58,0,0,0-8-1.37l-35.34,17a11.84,11.84,0,0,1-9,0l-35.34-17a7.58,7.58,0,0,0-8,1.37L19.45,223.53c-1.94,1.95-1.28,3.54,1.47,3.54H276.07c2.75,0,3.41-1.59,1.47-3.54Z" transform="translate(0 -69.93)"></path>
+                        <path d="M292.53,88.87l-79.09,39.58a3.39,3.39,0,0,0-.93,5.78l80.95,80.9c2,1.94,3.54,1.29,3.54-1.46v-122C297,88.88,295,87.63,292.53,88.87Z" transform="translate(0 -69.93)"></path>
+                        <path d="M9.33,72.17,144,139.58a11.21,11.21,0,0,0,8.94,0l134.7-67.42c2.45-1.23,2.22-2.23-.53-2.23H9.86C7.11,69.93,6.88,70.93,9.33,72.17Z" transform="translate(0 -69.93)"></path>
+                        </svg>
+                    </div>
+
+                    <div class="storeDetail">
+                        <a href="mailto:Email@example.com" aria-label="Store email address: Email@example.com">Email@example.com</a>
+                    </div>
+                    </li>
+
+                </ul>
+                </div><!--.footerStore-details ends-->
+            </div>
+
+        </div>
+    </div>
 </footer>
+</section>
